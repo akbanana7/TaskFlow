a95bdfee0ffebd354ecff01c6e6058351c701118
akbanana7 <akbanana70@gmail.com>
2026-08-30 17:46:49 +0100

    WAZAAAAA. Made python scream at you when you call localhost:8000/add. init formHandler. fleshed out the frontend a bit.

--- analytics/app.py
+++ analytics/app.py
@@ -1,1 +1,13 @@
-import matplotlib
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/add/")
+async def addToDb():
+    print("WAZAAAAAA")
+    return "WAZAAAA"
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
