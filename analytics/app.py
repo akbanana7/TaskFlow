c6d6bf8d206a1d82557f8d37d44375a3fbc601a3
akbanana7 <akbanana70@gmail.com>
2026-08-31 07:34:37 +0100

    added basic postgresql (database) integration into backend. added init api call (wipes table "main" and reinit's)

--- analytics/app.py
+++ analytics/app.py
@@ -1,8 +1,40 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import psycopg
+from dotenv import load_dotenv
+import os
 
+# Database is on 5324
+
+# All init
 app = FastAPI()
+load_dotenv()
+dbPass = os.getenv("DB_PASS")
+
+
+@app.get("/init/")
+async def initDb():
+    with psycopg.connect(f"dbname=TaskFlow user=postgres password={str(dbPass)}") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+DROP TABLE IF EXISTS main
+
+""")
+            conn.commit()
+            cur.execute("""
+CREATE TABLE main (
+"user" varchar(255),
+timeStart time,
+timeEnd time,
+timezone int,
+dateStart date,
+dateEnd date
+)
+""")
+
+            conn.commit()      
+    return("Database init attempted")
 
 @app.get("/add/")
 async def addToDb():
