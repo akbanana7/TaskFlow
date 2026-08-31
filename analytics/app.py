b9ee1f88bf8a6952dd09330a219b0a6f72f5981b
akbanana7 <akbanana70@gmail.com>
2026-08-31 11:48:56 +0100

    added "add" API to add tasks to server. Unsure if it works as I need to stich front and back end together via REST

--- analytics/app.py
+++ analytics/app.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import psycopg
 from dotenv import load_dotenv
 import os
+from datetime import date, time, datetime
 
 # Database is on 5324
 
@@ -13,8 +14,8 @@ load_dotenv()
 dbPass = os.getenv("DB_PASS")
 
 
-@app.get("/init/")
-async def initDb():
+@app.get("/init/") # For init database
+async def initDb(passWord: str = None):
     with psycopg.connect(f"dbname=TaskFlow user=postgres password={str(dbPass)}") as conn:
         with conn.cursor() as cur:
             cur.execute("""
@@ -36,10 +37,24 @@ dateEnd date
             conn.commit()      
     return("Database init attempted")
 
-@app.get("/add/")
-async def addToDb():
-    print("WAZAAAAAA")
-    return "WAZAAAA"
+@app.get("/add/") # For adding tasks
+async def addToDb(user: str = None, timeStart: str = None, timeEnd: str = None, timeZone: int = 0, dateStart: str = None, dateEnd: str = None):
+    with psycopg.connect(f"dbname=TaskFlow user=postgres password={str(dbPass)}") as conn:
+        with conn.cursor() as cur:
+            if user:
+                # Parse into objects to be sent to DB
+                try:
+                    timeStart = time.fromisoformat(timeStart)
+                    timeEnd = time.fromisoformat(timeStart)
+                    dateStart = date.fromisoformat(timeStart)
+                    dateEnd = date.fromisoformat(timeStart)
+
+                except:
+                    raise ValueError("Couldn't convert times")
+                cur.execute(f"""
+    INSERT INTO (user, timeStart, timeEnd, timezone, dateStart, dateEnd)
+    VALUES ({user}, {timeStart}, {timeEnd}, {timeZone}, {dateStart}, {dateEnd})
+""")
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
